40c9a4ae0913f87417a9c4f33dbb1828adb73ca3
mirage <119869686+ClementBobin@users.noreply.github.com>
2026-05-25 16:57:12 +0200

    feat: migrate utility functions from TypeScript to JavaScript and add Toaster component in main entry

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@
  */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from "@/components/ui/sonner"
 import './index.css'
 import App from './App'
 import "./lib/i18n";
@@ -21,6 +22,7 @@ async function bootstrap() {
   createRoot(document.getElementById("root")).render(
     <StrictMode>
       <App />
+      <Toaster />
     </StrictMode>
   );
 }
