c4620ee3b8108160041d228218464448f798a456
Nizard <nizard.verdenal@diiage.org>
2026-06-09 16:56:25 +0200

    fix: champs auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,8 +3,8 @@ import { apiClient } from "./client.js";
 export const loginUser = ({ email, password }) =>
   apiClient.post("/auth/login", { email, password });
 
-export const registerUser = ({ fullName, email, password }) =>
-  apiClient.post("/auth/register", { fullName, email, password });
+export const registerUser = ({ firstName, lastName, email, password }) =>
+  apiClient.post("/auth/register", { firstName, lastName, email, password });
 
 export const refreshToken = ({ refreshToken }) =>
   apiClient.post("/auth/refresh", { refreshToken });
